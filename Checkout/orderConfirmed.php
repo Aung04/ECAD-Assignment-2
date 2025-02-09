56d967516db3c250f05ec2647f4ddd5b06d34d14
Your own username <Your own email>
2025-02-09 16:44:21 +0800

    Move folder

--- Checkout/orderConfirmed.php
+++ Checkout/orderConfirmed.php
@@ -1,13 +1,13 @@
 <!-- Linh --->
-<?php 
+<?php
 session_start(); // Detect the current session
-include("header.php"); 
+include("navbar.php");
 
-if(isset($_SESSION["OrderID"])) {	
+if (isset($_SESSION["OrderID"])) {
 	echo "<p>Checkout successful. Your order number is $_SESSION[OrderID]</p>";
 	echo "<p>Thank you for your purchase.&nbsp;&nbsp;";
 	echo '<a href="index.php">Continue shopping</a></p>';
-} 
+}
 
-include("footer.php"); 
+include("footer.php");
 ?>
